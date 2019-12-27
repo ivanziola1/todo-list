435ee227ca6356177caa7937e5770e5a7043de7d
Ivan Ziola <ivan.ziola@webmil.com.ua>
2019-12-27 23:53:21 +0200

    add completed styles

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,10 +3,16 @@ import React, {Component} from "react"
 class TodoItem extends Component {
 
   render(){
+    const completeStyles = {
+      textDecoration: 'line-through',
+      fontStyle: 'italic',
+      color: "#cdcdcd"
+    }
+
     return(
       <div className="todo-item">
          <input type="checkbox" checked={this.props.item.completed} onChange={() => this.props.changeHandler(this.props.item.id)}/>
-         <p>{this.props.item.name}</p>
+         <p style={this.props.item.completed ? completeStyles : null}>{this.props.item.name}</p>
       </div>
     )
   }
